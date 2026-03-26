fbd6b782d6bcc91ead26b51fce233c5c60aeabda
Devops Factory In <38098820+devopsfactory-in@users.noreply.github.com>
2026-03-26 18:51:01 +0100

    Change client secret in AnalyticsAPI.py
    Update client secret to a new value.

--- AnalyticsAPI.py
+++ AnalyticsAPI.py
@@ -25,7 +25,7 @@ config.read('resources/configparams.ini')
 
 clientid=config['DEFAULT']['client_id']
 clientsecret=config['DEFAULT']['client_secret']
-clientsecret=124356567567
+clientsecret=1243565675679
 
   
 def createnewreportsuite():
